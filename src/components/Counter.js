1f0c54df26f924b5d487ddf9dd46afb56f4e874d
Nadir Kalajdzic <nadirka@maestralsolutions.com>
2021-08-20 19:13:51 +0200

    Implemented redux with toolkit
    - I already implemented redux
      with regular redux, but I am
      commiting now only the toolkit

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,12 +1,45 @@
-import classes from './Counter.module.css';
+import classes from "./Counter.module.css";
+import { useDispatch, useSelector } from "react-redux";
+import { counterActions } from "../store/index";
+
+import "./Counter.css";
 
 const Counter = () => {
-  const toggleCounterHandler = () => {};
+  const dispatch = useDispatch();
+  const counter = useSelector((state) => state.counter.counter);
+  const show = useSelector((state) => state.counter.showCounter);
+
+  const incrementHandler = () => {
+    dispatch(counterActions.increment());
+  };
+
+  const increaseHandler = () => {
+    dispatch(counterActions.increase(10));
+  };
+
+  const decrementHandler = () => {
+    dispatch(counterActions.decrement());
+  };
+
+  const toggleCounterHandler = () => {
+    dispatch(counterActions.toggleCounter());
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      {show && <div className={classes.value}>{counter}</div>}
+      <div>
+        <button className="counter-button" onClick={incrementHandler}>
+          Increment
+        </button>
+        <button className="counter-button" onClick={increaseHandler}>
+          Increase by 10
+        </button>
+        <button className="counter-button" onClick={decrementHandler}>
+          Decrement
+        </button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
